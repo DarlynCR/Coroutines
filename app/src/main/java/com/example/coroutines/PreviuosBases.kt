588684461299dd40a89b87c154fb698c2822a585
DarlynCaicedo <darlyn.caicedo.calderon@gmail.com>
2022-04-13 17:26:37 -0500

    Tipos de constructores de corrutinas

--- app/src/main/java/com/example/coroutines/PreviuosBases.kt
+++ app/src/main/java/com/example/coroutines/PreviuosBases.kt
@@ -1,6 +1,98 @@
 package com.example.coroutines
 
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
 fun main() {
 
-    println("comencemos")
+    // lambdas
+    val result = operations(3,2)  { y, x ->  y * x }
+
+    //Corrutinas -> primer ejemplo
+    //coroutines()
+
+    // Coonstructor de corrutinas GlobalScope
+    globalScope()
+
+    readLine()
+    //Usado en testing y en enseñanza, este constructor bloquea el hilo principal
+    cRunBloking()
+    // Usado para tareas donde no se espere un resultado
+    cLaunch() //Retorna un Job
+    // Diseñado para cuando esperemos un valor en la corrutina
+    cAsync() //Retorna un deferred
+    //
+
+
+
 }
+
+fun cAsync() {
+    runBlocking {
+       val result = async {
+            startMsg()
+            println("Async...")
+            endMsg()
+            operations(2,5){y,x -> y+5}
+        }
+
+        println("Async result = ${result.await()}")
+    }
+}
+
+fun cRunBloking() {
+    runBlocking {
+        startMsg()
+        println("RunBloking...")
+        endMsg()
+    }
+}
+
+fun cLaunch() {
+    runBlocking {
+        launch {
+            startMsg()
+            println("Launch...")
+            endMsg()
+        }
+    }
+}
+
+fun globalScope() {
+
+    GlobalScope.launch{
+        startMsg()
+        println("GlobalScope")
+        endMsg()
+    }
+
+
+
+}
+
+fun endMsg() {
+    println("Corrutina finalizada: -${Thread.currentThread().name}-")
+}
+
+fun startMsg() {
+    println("Comenzando corrutina: -${Thread.currentThread().name}-")
+}
+
+fun coroutines() {
+    //Primer ejemplo de corrutina
+    runBlocking {
+        (1..1_000_000).forEach {
+            launch {
+                delay(1000)
+                print("*")
+            }
+        }
+    }
+}
+
+
+
+//Función de orden superior
+fun operations (y : Int , x: Int, block : (Int, Int) -> Int) = block(y,x)
+
